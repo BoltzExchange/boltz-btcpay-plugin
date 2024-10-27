e22738de91b797f83ce6cee50d301dd9f0e16b9c
jackstar12 <jkranawetter05@gmail.com>
2024-10-27 11:23:52 +0100

    chore: add submodule init to release script

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
+git submodule update --init
 dotnet publish BTCPayServer.Plugins.Boltz -o ./publish
 dotnet run --project btcpayserver/BTCPayServer.PluginPacker ./publish BTCPayServer.Plugins.Boltz ./release
