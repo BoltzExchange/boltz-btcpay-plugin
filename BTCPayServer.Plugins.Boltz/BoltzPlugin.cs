cd958af9767c3cad0a9f4f09b5f1352ba7dfd29d
jackstar12 <jkranawetter05@gmail.com>
2024-11-21 21:48:52 +0100

    Revert "fix: add <2.0 version btcpay version constraint"
    This reverts commit 811707b483006181f112e1b3e0831cf868a2b05d.
    BTCPay implementation of operator is broken.

--- BTCPayServer.Plugins.Boltz/BoltzPlugin.cs
+++ BTCPayServer.Plugins.Boltz/BoltzPlugin.cs
@@ -19,8 +19,7 @@ public class BoltzPlugin : BaseBTCPayServerPlugin
 
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     [
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" },
-        new() { Identifier = nameof(BTCPayServer), Condition = "<2.0.0" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
     ];
 
     public override void Execute(IServiceCollection services)
