0d0fe85113f5a5985a1fa8a552aa97e205a5d4b5
jackstar12 <jkranawetter05@gmail.com>
2025-01-03 11:16:56 +0100

    chore: bump version to v2.1.6

--- BTCPayServer.Plugins.Boltz/BoltzPlugin.cs
+++ BTCPayServer.Plugins.Boltz/BoltzPlugin.cs
@@ -13,7 +13,7 @@ namespace BTCPayServer.Plugins.Boltz;
 
 public class BoltzPlugin : BaseBTCPayServerPlugin
 {
-    public override Version Version => new(2, 1, 5);
+    public override Version Version => new(2, 1, 6);
 
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     [
