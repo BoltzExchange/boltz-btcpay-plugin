a14a690f6217f145e7a16053b8ec5a558c3249dd
jackstar12 <jkranawetter05@gmail.com>
2025-02-13 20:02:56 +0100

    chore: bump client to v2.3.9

--- BTCPayServer.Plugins.Boltz/BoltzDaemon.cs
+++ BTCPayServer.Plugins.Boltz/BoltzDaemon.cs
@@ -55,7 +55,7 @@ public class BoltzDaemon(
     BTCPayNetworkProvider btcPayNetworkProvider
 )
 {
-    private static readonly Version ClientVersion = new("2.3.7");
+    private static readonly Version ClientVersion = new("2.3.9");
 
     private Stream? _downloadStream;
     private Task? _startTask;
