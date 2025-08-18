4f4eae59e815114154c8f3b96e428087ff1218c6
michael1011 <me@michael1011.at>
2025-08-18 22:38:20 +0200

    Update BTCPayServer.Plugins.Boltz/BoltzClient.cs

--- BTCPayServer.Plugins.Boltz/BoltzClient.cs
+++ BTCPayServer.Plugins.Boltz/BoltzClient.cs
@@ -180,7 +180,7 @@ public class BoltzClient : IDisposable
 
     public async Task<Wallet> ImportWallet(WalletParams @params, WalletCredentials credentials)
     {
-        // We dont add the default timeout here because it can take quite a bit if the wallet has a big tx history
+        // We do not add the default timeout here because it can take quite a bit if the wallet has a big tx history
         return await _client.ImportWalletAsync(new ImportWalletRequest { Params = @params, Credentials = credentials },
             CreateCallOptions(CancellationToken.None));
     }
